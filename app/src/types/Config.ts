5f074e65ad64ad0c802b3bbbd7930ffaeb1a4660
Krists Pudzens <kpudzens@gmail.com>
2026-02-06 06:50:35 +0200

    a

--- app/src/types/Config.ts
+++ app/src/types/Config.ts
@@ -1,10 +1,16 @@
 import type { CameraConfig, Vector } from "@src/types/CameraConfig";
 import { type Component } from "vue";
 
+export interface HilightedBones {
+    name: string;
+    text: string;
+    meshes: string[];
+}
+
 export default interface Config {
     cameraConfig: Record<string, CameraConfig>;
     pivot: Vector;
-    hilightedBones: Object[];
+    hilightedBones: HilightedBones[];
     animationComponent: Component | null;
     animationTitle: string | null;
 };
